5f48d7bd624ba161a33890201ffa553487d8af92
Andre1999Lopes <andrejuniorlopes@gmail.com>
2021-04-20 17:50:12 -0300

    Merge branch 'gh-pages'

--- src/store/hookStore.ts
+++
@@ -1,7 +0,0 @@
-import { createTypedHooks } from 'easy-peasy';
-import { IBlockModel } from './storeModel';
-
-const typedHooks = createTypedHooks<IBlockModel>();
-
-export const useStoreState = typedHooks.useStoreState;
-export const useStoreActions = typedHooks.useStoreActions;
--- src/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'easy-peasy';
-import { BlockModel } from './storeModel';
-
-const store = createStore(BlockModel);
-
-export default store;
